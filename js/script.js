a7bde420a9b59a27ab99776dee8edef19474365c
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-30 16:08:35 +0200

    fix & add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-// Attraverso l'apposita API di Boolean
-// https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all'interno di una lista
-
+const app = new Vue({
+    el: '#root',
+    data: {
+        contacts: [
+            mail="",
+        ]
+    },
+    created() {
+        for(let i = 0 ; i < 10 ; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.contacts.push(response.data.response);
+            })
+        }
+    },
+});
