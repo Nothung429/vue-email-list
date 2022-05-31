f3904606a3a58cb46711bacbe2ed0ad960512dab
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-31 10:17:42 +0200

    general fix

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const app = new Vue({
         contacts: [ ],
     },
     created() {
-        for(let i = 0 ; i <= 10 ; i++) {
+        for(let i = 0 ; i < 10 ; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
                 this.contacts.push(response.data.response);
